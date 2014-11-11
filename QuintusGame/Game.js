24c2f50c612f448eb6effc329d50ba345f5bd74e
leafyfresh <nmnoonan@hotmail.com>
2014-11-11 14:58:31 -0600

    more files
    adding more files

---
+++ QuintusGame/Game.js
@@ -0,0 +1,14 @@
+﻿var Q = Quintus({ development: true })
+.include("Sprites")
+.setup({ width: 800, height: 480 });
+
+
+
+
+Q.load(["starBackground.png"], function () {
+    var background = new Q.Sprite({ asset: "starBackground.png", x: Q.el.width / 2, y: Q.el.height / 2, type: Q.SPRITE_NONE })
+    Q.gameLoop(function (dt) {
+        Q.clear();
+        background.render(Q.ctx);
+    });
+});
