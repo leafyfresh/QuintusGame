8d370c0dd03698fd1a75ef5e5ea3ae12a95c5165
leafyfresh <nmnoonan@hotmail.com>
2014-12-08 17:37:32 -0600

    Added animation, fixed player display bug

--- QuintusGame/Game.js
+++ QuintusGame/Game.js
@@ -1,39 +1,37 @@
 ﻿var Q = Quintus({ development: true })
-.include("Sprites")
+.include("Sprites, Anim")
 .setup({ width: 800, height: 480 });
 
 
 Q.Sprite.extend("Player", {
     init: function (p) {
         this._super(p, {
-            asset: "cheat.png",
+            // asset: "cheet.png",
+            sprite: "4cheets",
+            sheet: "4cheets",
             x: Q.el.width / 2,
             y: Q.el.height - 60,
             type: Q.SPRITE_FRIENDLY,
             speed:10
         });
+
+        this.add("animation");
+        this.play("default");
     }
 });
 
-Q.Sprite.extend("Penguin", {
-    init: function (p) {
-        this._super({
-            asset: "cheat.png",
-            cx: 0,
-            cy:0
-        });
-    }
-});
 
-Q.load(["starBackground.png"], function () {
+Q.load(["starBackground.png", "cheet.png","4cheets.png", "4cheets.json"], function () {
+   Q.compileSheets("4cheets.png", "4cheets.json");
+    Q.animations("4cheets", {default: {frames:[0,1,3,2], rate: 1 }})
+
     var background = new Q.Sprite({ asset: "starBackground.png", x: Q.el.width / 2, y: Q.el.height / 2, type: Q.SPRITE_NONE })
-    penguin = new Q.Penguin();
-    debugger;
-    penguin.render(Q.ctx);
+    var player = new Q.Player();
     Q.gameLoop(function (dt) {
         Q.clear();
         background.render(Q.ctx);
-        alert("test - delete me")
-        alert("test -  yes delete me")
+        player.update(dt);
+        player.render(Q.ctx);
+       
     });
 });
