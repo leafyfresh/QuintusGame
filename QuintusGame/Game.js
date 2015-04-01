361e0d57ec24b0d4d4cd73f1718b6af23aadd473
leafyfresh <nmnoonan@hotmail.com>
2015-04-01 18:12:50 -0500

    User Input and dynamic player images

--- QuintusGame/Game.js
+++ QuintusGame/Game.js
@@ -1,14 +1,16 @@
 ﻿var Q = Quintus({ development: true })
-.include("Sprites, Anim")
-.setup({ width: 800, height: 480 });
+.include("Sprites, Anim, Input, Touch")
+.setup({ width: 800, height: 480 })
+.controls();
 
 
 Q.Sprite.extend("Player", {
     init: function (p) {
+       // debugger;
         this._super(p, {
-            // asset: "cheet.png",
-            sprite: "4cheets",
-            sheet: "4cheets",
+             asset: p,
+          ////  sprite: "4cheets",
+          // sheet: "4cheets",
             x: Q.el.width / 2,
             y: Q.el.height - 60,
             type: Q.SPRITE_FRIENDLY,
@@ -16,7 +18,8 @@ Q.Sprite.extend("Player", {
         });
 
         this.add("animation");
-        this.play("default");
+        this.play("");
+        // this.play("default");
     }
 });
 
@@ -26,12 +29,21 @@ Q.load(["starBackground.png", "cheet.png","4cheets.png", "4cheets.json"], functi
     Q.animations("4cheets", {default: {frames:[0,1,3,2], rate: 1 }})
 
     var background = new Q.Sprite({ asset: "starBackground.png", x: Q.el.width / 2, y: Q.el.height / 2, type: Q.SPRITE_NONE })
-    var player = new Q.Player();
+    var player = new Q.Player("cheet.png");
+    var playerTwo = new Q.Player('redcheet.png');
     Q.gameLoop(function (dt) {
+      //  console.log('looping..');
         Q.clear();
         background.render(Q.ctx);
         player.update(dt);
         player.render(Q.ctx);
-       
+        if (Q.inputs['left']){
+            console.log("left!");
+            player.p.x--;
+        }
+        if (Q.inputs['right']){
+            console.log("right!");
+            player.p.x++;
+        }
     });
 });
